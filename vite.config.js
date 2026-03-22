080a043effae1e74209822296f1625a8d9d1c9df
lepilo00 <s.dedjanski@gmail.com>
2026-03-22 18:24:19 +0100

    #small fix for netlify

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
+        manualChunks: (id) => {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor';
+          }
         },
       },
     },
